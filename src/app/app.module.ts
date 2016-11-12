ccf7d0e9972a72e2e5378fa7ae715aee175792c7
Amaury <amauryleclerc@hotmail.fr>
2016-11-12 20:49:15 +0100

    nettoyage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from "./app.component";
-import {TaskListComponent} from "./todo/components/task-list.component";
-import {AboutComponent} from "./about/components/about.component";
-import {TaskComponent} from "./todo/components/task.component";
 import {MainComponent} from "./main/components/main.component";
 import {MapComponent} from "./main/components/map.component";
 import {routing, appRoutingProviders} from './app.routing';
@@ -31,9 +28,6 @@ const WINDOW_PROVIDER: ValueProvider = {
     ],
     declarations: [
         AppComponent,
-        TaskComponent,
-        TaskListComponent,
-        AboutComponent,
         MainComponent,
         MapComponent
     ],
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,14 +1,10 @@
 import {Routes, RouterModule} from "@angular/router";
-import {TaskListComponent} from "./todo/components/task-list.component";
-import {AboutComponent} from "./about/components/about.component";
 import {MainComponent} from "./main/components/main.component";
 import {ModuleWithProviders} from "@angular/core";
 
 const appRoutes: Routes = [
     {path: '', redirectTo: 'main', pathMatch: 'full'},
-    {path: 'main', component: MainComponent},
-    {path: 'tasks', component: TaskListComponent, data: {title: 'TaskList'}},
-    {path: 'about', component: AboutComponent, data: {title: 'About'}}
+    {path: 'main', component: MainComponent, data: {title: 'WindTrack'}}
 ];
 
 export const appRoutingProviders: any[] = [];
